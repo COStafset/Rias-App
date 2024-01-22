4c99da6cafec50b4d1293ad9a498641ddd9160c4
Christian <costafset@gmail.com>
2024-01-22 14:28:24 +0100

    Yessir

---
+++ RiasServer/src/main/java/no/mysite/rias/server/RiasServerApplication.java
@@ -0,0 +1,13 @@
+package no.mysite.rias.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiasServerApplication.class, args);
+    }
+
+}
